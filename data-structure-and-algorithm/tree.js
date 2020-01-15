e32c403410757fcd13d1b9c8affcb418bb8bee80
qiugu <476710564@qq.com>
2020-01-15 22:03:21 +0800

    feat: tree delete operate

--- data-structure-and-algorithm/tree.js
+++ data-structure-and-algorithm/tree.js
@@ -54,6 +54,112 @@ BST.prototype.postOrder = function() {
   postOrderTraverse(this.root)
 }
 
+BST.prototype.min = function() {
+  let node = this.root
+  while (true) {
+    node = node.left
+    if (node.left === null) {
+      return node.data
+    }
+  }
+}
+
+BST.prototype.max = function() {
+  let node = this.root
+  let parent
+  while (node !== null) {
+    parent = node
+    node = node.right
+  }
+  return parent.data
+}
+
+BST.prototype.has = function(data) {
+  let node = this.root
+
+  while (node !== null) {
+    if (data < node.data) {
+      node = node.left
+    } else if (data > node.data) {
+      node = node.right
+    } else {
+      return true
+    }
+  }
+
+  return false
+}
+
+BST.prototype.remove = function(data) {
+  let current = this.root
+  // 保存删除节点的父节点以及删除节点位于父节点的左边还是右边
+  let parent, isLeft
+
+  // 查找要删除的值
+  while (current.data !== data) {
+    parent = current
+    if (data < current.data) {
+      current = current.left
+      isLeft = true
+    } else {
+      current = current.right
+      isLeft = false
+    }
+  }
+
+  // 如果删除的值不存在
+  if (current === null) return false
+
+  // 如果删除的是叶子节点
+  if (current.left === null && current.right === null) {
+    // 如果删除的节点是根节点
+    // 如果删除的节点是父节点的左子节点
+    // 如果删除的节点是父节点的右子节点
+    if (current === this.root) {
+      this.root = null
+    } else if (isLeft) {
+      parent.left = null
+    } else {
+      parent.right = null
+    }
+  }
+
+  // 如果删除的节点只有一个子节点
+  else if (current.left === null) {
+    if (current === this.root) {
+      this.root = current.right
+    } else if (isLeft) {
+      parent.left = current.right
+    } else {
+      parent.right = current.right
+    }
+  }
+  else if (current.right === null) {
+    if (current === this.root) {
+      this.root = current.left
+    } else if (isLeft) {
+      parent.left = current.left
+    } else {
+      parent.right = current.left
+    }
+  }
+
+  // 删除的节点有两个子节点（查找后继节点为例）
+  // 需要改变父节点的指向，指向后继节点
+  // 需要将后继节点的左子节点指向删除节点的左子节点，
+  else {
+    const subsequent = getSubsequent(current)
+    if (current === this.root) {
+      this.root = subsequent
+    } else if (isLeft) {
+      parent.left = subsequent
+    } else {
+      parent.right = subsequent
+    }
+    subsequent.left = current.left
+  }
+}
+
 function insertNode(current, newNode) {
   if (newNode.data < current.data) {
     if (current.left === null) {
@@ -94,6 +200,31 @@ function postOrderTraverse(node) {
   }
 }
 
+// 查找当前节点的后继节点
+// 使用后继节点来替换删除的节点，并改变节点指向
+function getSubsequent(node) {
+  // 保存后继节点、后继节点的父节点
+  let subsequentParent
+  let subsequent = node
+  let current = node.right
+
+  while (current !== null) {
+    subsequentParent = subsequent
+    subsequent = current
+    current = current.left
+  }
+
+  // 如果查找的后继节点不是需要删除节点的右节点
+  if (subsequent !== node.right) {
+    // 将后继节点的父节点的左子节点指向后继节点的右子节点
+    subsequentParent.left = subsequent.right
+    // 将后继节点的右子节点指向删除节点的右子节点
+    subsequent.right = node.right
+  }
+
+  return subsequent
+}
+
 // 测试
 const bst = new BST()
 bst.insert(23)
@@ -106,3 +237,6 @@ bst.insert(22)
 bst.preOrder(23)
 bst.inOrder(23)
 bst.postOrder(23)
+console.log(bst.min())
+console.log(bst.max())
+console.log(bst.has(45))
