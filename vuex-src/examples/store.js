c23cd00ad358d1fff79dea226f18b1e70721a083
qiugu <476710564@qq.com>
2020-01-16 16:47:05 +0800

    feat: analysis vuex process

--- vuex-src/examples/store.js
+++ vuex-src/examples/store.js
@@ -3,9 +3,22 @@ import Vue from 'vue'
 
 Vue.use(Vuex)
 
+const main = {
+  state: {
+    grandson: 'qqq'
+  }
+}
+
 const app = {
+  namespaced: true,
+  modules: {
+    main
+  },
   state: {
     count: 0
+  },
+  getters: {
+    count: state => state.count
   },
   mutations: {
     ADD_COUNT (state, payload) {
@@ -17,5 +30,20 @@ const app = {
 export default new Vuex.Store({
   modules: {
     app
+  },
+  state: {
+    sessionId: 'abc'
+  },
+  mutations: {
+    CHANGE (state, payload) {
+      state.sessionId = payload
+    }
+  },
+  actions: {
+    changeSession ({ commit }) {
+      setTimeout(() => {
+        commit('CHANGE')
+      }, 3000)
+    }
   }
 })
--- vuex-src/src/module/module-collection.js
+++ vuex-src/src/module/module-collection.js
@@ -3,11 +3,19 @@ import { forEachValue } from '../util'
 
 export default class ModuleCollection {
   constructor (rawRootModule) {
+    // 注册根模块，也就是new Vuex.Store()中传入的options
     this.register([], rawRootModule, false)
   }
 
+  /**
+   * 注册模块的方法，如果是注册根模块，路径为[]，嵌套模块路径存储在path中
+   * @param {array} path 模块的路径，如果有子路径则加入path数组中 
+   * @param {object} rawModule Vuex.Store中的options
+   * @param {boolean} runtime 
+   */
   register (path, rawModule, runtime = true) {
     const newModule = new Module(rawModule, runtime)
+
     if (path.length === 0) {
       this.root = newModule
     } else {
@@ -23,14 +31,24 @@ export default class ModuleCollection {
     }
   }
 
-  unregister (path) {}
+  // 卸载模块
+  unregister (path) {
+    const parent = this.get(path.slice(0, -1))
+    const key = path[path.length - 1]
+    // 如果模块是Vuex.Store中注册的模块，无法卸载
+    if (!parent.getChild(key).runtime) return
 
+    parent.removeChild(key)
+  }
+
+  // 获取路径下的模块
   get (path) {
     return path.reduce((module, key) => {
       return module.getChild(key)
     }, this.root)
   }
 
+  // 获取路径下的模块的命名空间
   getNamespace (path) {
     let module = this.root
     return path.reduce((namespace, key) => {
@@ -39,5 +57,23 @@ export default class ModuleCollection {
     }, '')
   }
 
-  update (rawRootModule) {}
+  // 模块热重载功能
+  update (rawRootModule) {
+    update([], this.root, rawRootModule)
+  }
+}
+
+function update (path, targetModule, newModule) {
+  targetModule.update(newModule)
+
+  // 如果有子模块，递归更新子模块
+  if (newModule.modules) {
+    for (const key in newModule.modules) {
+      update(
+        path.concat(key),
+        targetModule.getChild(key),
+        newModule.modules[key]
+      )
+    }
+  }
 }
